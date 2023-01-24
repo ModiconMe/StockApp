0f19da05b8e461cfe9f9c5dd58c3be855b8d3965
saeed <rebeelsc@gmail.com>
2023-01-24 14:33:21 +0700

    change build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 ./gradlew clean build
 docker build -t modiconme/tinkoff-service:latest TinkoffService/
-docker build -t modiconme/tinkoff-service:latest MoexService/
+docker build -t modiconme/moex-service:latest MoexService/
 docker login -u $1 -p $2
 docker push modiconme/tinkoff-service:latest
 docker push modiconme/moex-service:latest

5c394ecb79cf160c1f1a28a5e19f36f8d97e35e9
saeed <rebeelsc@gmail.com>
2023-01-26 23:10:37 +0700

    repair docker compose, update PriceService to boostrap Redis in container correctly

--- build-local.sh
+++ build-local.sh
@@ -5,10 +5,4 @@ docker build -t modiconme/tinkoff-service:latest TinkoffService/
 docker build -t modiconme/moex-service:latest MoexService/
 docker build -t modiconme/user-service:latest UserService/
 docker build -t modiconme/price-service:latest PriceService/
-docker login -u $1 -p $2
-docker push modiconme/stock-service:latest
-docker push modiconme/tinkoff-service:latest
-docker push modiconme/moex-service:latest
-docker push modiconme/user-service:latest
-docker push modiconme/price-service:latest
 

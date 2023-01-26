089019e1e05d29d2bf2a56f3fe0d36ee54c85cd1
saeed <rebeelsc@gmail.com>
2023-01-26 20:55:25 +0700

    add test for PriceServiceClient in GetStockWithPricesHandler

--- StockService/stock-service/src/test/java/io/modicon/stockservice/application/query/GetStocksWithPricesHandlerTest.java
+++ StockService/stock-service/src/test/java/io/modicon/stockservice/application/query/GetStocksWithPricesHandlerTest.java
@@ -1,9 +1,11 @@
 package io.modicon.stockservice.application.query;
 
+import io.modicon.priceservice.api.query.GetStocksWithPricesFromRedisResult;
 import io.modicon.stockservice.api.dto.StockWithPriceDto;
 import io.modicon.stockservice.api.query.GetStocksWithPrices;
 import io.modicon.stockservice.api.query.GetStocksWithPricesResult;
 import io.modicon.stockservice.application.EntitySource;
+import io.modicon.stockservice.application.client.PriceServiceClient;
 import io.modicon.stockservice.application.service.MoexStockPriceService;
 import io.modicon.stockservice.application.service.TinkoffStockPriceService;
 import org.junit.jupiter.api.BeforeEach;
@@ -26,19 +28,23 @@ class GetStocksWithPricesHandlerTest {
     private TinkoffStockPriceService tinkoffStockPriceService;
     @Mock
     private MoexStockPriceService moexStockPriceService;
+    @Mock
+    private PriceServiceClient priceServiceClient;
 
     private GetStocksWithPricesHandler getStocksWithPricesHandler;
     private EntitySource entitySource = new EntitySource();
 
     @BeforeEach
     void setUp() {
-        getStocksWithPricesHandler = new GetStocksWithPricesHandler(tinkoffStockPriceService, moexStockPriceService);
+        getStocksWithPricesHandler = new GetStocksWithPricesHandler(tinkoffStockPriceService, moexStockPriceService, priceServiceClient);
     }
 
     @Test
     void should_returnCorrectData_whenOnlyTinkoffStocks() {
         when(tinkoffStockPriceService.getStocksWithPrices(any()))
                 .thenReturn(entitySource.stocksWithPrice);
+        when(priceServiceClient.getPricesFromCache(any()))
+                .thenReturn(new GetStocksWithPricesFromRedisResult(new HashSet<>()));
 
         GetStocksWithPricesResult result = getStocksWithPricesHandler.handle(new GetStocksWithPrices(entitySource.figis));
 
@@ -54,6 +60,21 @@ class GetStocksWithPricesHandlerTest {
                 .thenReturn(new HashSet<>());
         when(moexStockPriceService.getStocksWithPrices(any()))
                 .thenReturn(entitySource.stocksWithPrice);
+        when(priceServiceClient.getPricesFromCache(any()))
+                .thenReturn(new GetStocksWithPricesFromRedisResult(new HashSet<>()));
+
+        GetStocksWithPricesResult result = getStocksWithPricesHandler.handle(new GetStocksWithPrices(entitySource.figis));
+
+        Set<StockWithPriceDto> stocks = new HashSet<>(result.getStocks());
+        assertThat(stocks).isNotEmpty();
+        assertThat(stocks.size()).isEqualTo(entitySource.stocksWithPrice.size());
+        assertThat(stocks).isEqualTo(entitySource.stocksWithPrice);
+    }
+
+    @Test
+    void should_returnCorrectData_whenOnlyCache() {
+        when(priceServiceClient.getPricesFromCache(any()))
+                .thenReturn(new GetStocksWithPricesFromRedisResult(entitySource.stocksWithPrice));
 
         GetStocksWithPricesResult result = getStocksWithPricesHandler.handle(new GetStocksWithPrices(entitySource.figis));
 
